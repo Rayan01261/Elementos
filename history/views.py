08e4f5e17111a2985876a7473bb3f8aa41c7d580
Rayan01261 <rayan012@outlook.com>
2025-03-31 12:28:16 -0300

    Container do docker criado.

--- history/views.py
+++ history/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Partida
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+@login_required
 def index(request):
-    partidas = Partida.objects.all()
+    partidas = Partida.objects.filter(
+        jogador1=request.user
+    ).union(
+        Partida.objects.filter(jogador2=request.user)
+    )  # Ordem decrescente
     context = {"partidas": partidas}
     return render(request, "history/index.html", context)
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Jogador
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def index(request):
     jogadores = Jogador.objects.order_by('-vitorias')  # Ordem decrescente
     context = {"jogadores": jogadores}
